274f5aed679918b6e2dccd499924291adcf7486c
Bojan Ristic <e.tachka@gmail.com>
2026-01-06 16:56:42 +0100

    webgpu

--- webclient/snac-processor.js
+++ webclient/snac-processor.js
@@ -1,38 +1,62 @@
-// snac-processor.js
 class SNACProcessor extends AudioWorkletProcessor {
     constructor() {
         super();
-        this.audioBuffer = new Float32Array(0);
+        // 2 seconds @ 24kHz is 48,000 samples. Correct.
+        this.buffer = new Float32Array(48000);
+        this.readIndex = 0;
+        this.writeIndex = 0;
+        this.isBuffering = true;
+
+        // Change from 0.34 (340ms) to 0.1 (100ms)
+        this.bufferThreshold = 24000 * 0.1;
 
         this.port.onmessage = (e) => {
             if (e.data.type === 'PCM_DATA') {
-                const newAudio = e.data.data;
-                if (!(newAudio instanceof Float32Array)) {
-                    console.error("Worklet expected Float32Array, got:", typeof newAudio);
-                    return;
+                const data = e.data.data;
+
+                // Use .set() for fast block copying
+                if (this.writeIndex + data.length < this.buffer.length) {
+                    this.buffer.set(data, this.writeIndex);
+                    this.writeIndex += data.length;
+                } else {
+                    // Handle circular wrap-around
+                    const firstPart = this.buffer.length - this.writeIndex;
+                    this.buffer.set(data.subarray(0, firstPart), this.writeIndex);
+                    this.buffer.set(data.subarray(firstPart), 0);
+                    this.writeIndex = data.length - firstPart;
                 }
-                this.appendAudio(e.data.data);
-            } else if (e.data.type === 'PAUSE') {
-                const silentSamples = new Float32Array(Math.floor(24000 * e.data.duration));
-                this.appendAudio(silentSamples);
             }
         };
     }
 
-    appendAudio(newAudio) {
-        const combined = new Float32Array(this.audioBuffer.length + newAudio.length);
-        combined.set(this.audioBuffer);
-        combined.set(newAudio, this.audioBuffer.length);
-        this.audioBuffer = combined;
+    writeToBuffer(data) {
+        for (let i = 0; i < data.length; i++) {
+            this.buffer[this.writeIndex] = data[i];
+            this.writeIndex = (this.writeIndex + 1) % this.buffer.length;
+        }
     }
 
     process(inputs, outputs) {
         const output = outputs[0][0];
-        if (this.audioBuffer.length >= output.length) {
-            output.set(this.audioBuffer.subarray(0, output.length));
-            this.audioBuffer = this.audioBuffer.subarray(output.length);
+        let available = (this.writeIndex - this.readIndex + this.buffer.length) % this.buffer.length;
+
+        // If we have data, play it. Only enter 'isBuffering' if we are COMPLETELY empty.
+        if (available < output.length) {
+            if (!this.isBuffering) {
+                console.warn("Underflow! Coasting...");
+                this.isBuffering = true;
+            }
+            output.fill(0);
+            return true;
+        }
+
+        // Once we've started playing, don't stop unless we hit 0 samples
+        for (let i = 0; i < output.length; i++) {
+            output[i] = this.buffer[this.readIndex];
+            this.readIndex = (this.readIndex + 1) % this.buffer.length;
         }
         return true;
     }
 }
+
 registerProcessor('snac-processor', SNACProcessor);
