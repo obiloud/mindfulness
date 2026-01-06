c55e7abb39d195a370422a905203738bfde713b8
Bojan Ristic <e.tachka@gmail.com>
2026-01-06 16:56:42 +0100

    webgpu

--- webclient/snac-worker.js
+++ webclient/snac-worker.js
@@ -42,23 +42,7 @@ onmessage = async (e) => {
         if (!session) {
             queue.push(e.data.tokens);
         } else {
-
-            // CHECK: Is 'data' a list of lists?
-            if (Array.isArray(e.data.tokens[0])) {
-                // It's a batch! Process each frame inside
-                for (const frame of e.data.tokens) {
-                    try {
-                        await performDecode(frame);
-                    } catch (err) {
-                        console.error("Frame Decode Error:", err);
-                    }
-                }
-            } else {
-                // It's a single frame (fallback)
-                await performDecode(e.data.tokens);
-            }
-
-
+            await performDecode(e.data.tokens);
         }
     }
 };
