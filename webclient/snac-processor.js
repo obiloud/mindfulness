4b65982a45bed9d47aa6f1636e798a9721e32ed4
Bojan Ristic <e.tachka@gmail.com>
2026-01-07 20:07:42 +0100

    buffering

--- webclient/snac-processor.js
+++ webclient/snac-processor.js
@@ -1,30 +1,20 @@
 class SNACProcessor extends AudioWorkletProcessor {
     constructor() {
         super();
-        // 2 seconds @ 24kHz is 48,000 samples. Correct.
-        this.buffer = new Float32Array(48000);
+        // 2 seconds @ 24kHz. 
+        this.bufferSize = 48000;
+        this.buffer = new Float32Array(this.bufferSize);
         this.readIndex = 0;
         this.writeIndex = 0;
         this.isBuffering = true;
 
-        // Change from 0.34 (340ms) to 0.1 (100ms)
-        this.bufferThreshold = 24000 * 0.1;
+        // FIXED: Lower threshold (approx 200ms) to avoid deadlock
+        // 24000 * 0.2 = 4,800 samples
+        this.bufferThreshold = 24000 * 0.5;
 
         this.port.onmessage = (e) => {
             if (e.data.type === 'PCM_DATA') {
-                const data = e.data.data;
-
-                // Use .set() for fast block copying
-                if (this.writeIndex + data.length < this.buffer.length) {
-                    this.buffer.set(data, this.writeIndex);
-                    this.writeIndex += data.length;
-                } else {
-                    // Handle circular wrap-around
-                    const firstPart = this.buffer.length - this.writeIndex;
-                    this.buffer.set(data.subarray(0, firstPart), this.writeIndex);
-                    this.buffer.set(data.subarray(firstPart), 0);
-                    this.writeIndex = data.length - firstPart;
-                }
+                this.writeToBuffer(e.data.data);
             }
         };
     }
@@ -32,29 +22,42 @@ class SNACProcessor extends AudioWorkletProcessor {
     writeToBuffer(data) {
         for (let i = 0; i < data.length; i++) {
             this.buffer[this.writeIndex] = data[i];
-            this.writeIndex = (this.writeIndex + 1) % this.buffer.length;
+            this.writeIndex = (this.writeIndex + 1) % this.bufferSize;
         }
     }
 
     process(inputs, outputs) {
         const output = outputs[0][0];
-        let available = (this.writeIndex - this.readIndex + this.buffer.length) % this.buffer.length;
+        if (!output) return true;
 
-        // If we have data, play it. Only enter 'isBuffering' if we are COMPLETELY empty.
-        if (available < output.length) {
-            if (!this.isBuffering) {
-                console.warn("Underflow! Coasting...");
-                this.isBuffering = true;
+        // Calculate how many samples are currently waiting
+        const available = (this.writeIndex - this.readIndex + this.bufferSize) % this.bufferSize;
+
+        // 1. Buffering Logic
+        if (this.isBuffering) {
+            if (available >= this.bufferThreshold) {
+                this.isBuffering = false;
+                console.log("Buffer ready, starting playback...");
+            } else {
+                output.fill(0);
+                return true;
             }
+        }
+
+        // 2. Underflow Protection
+        if (available < output.length) {
+            console.warn("Underflow detected, re-buffering...");
+            this.isBuffering = true;
             output.fill(0);
             return true;
         }
 
-        // Once we've started playing, don't stop unless we hit 0 samples
+        // 3. Playback
         for (let i = 0; i < output.length; i++) {
             output[i] = this.buffer[this.readIndex];
-            this.readIndex = (this.readIndex + 1) % this.buffer.length;
+            this.readIndex = (this.readIndex + 1) % this.bufferSize;
         }
+
         return true;
     }
 }
